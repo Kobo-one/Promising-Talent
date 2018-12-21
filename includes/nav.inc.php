5815b012a1a1afc1b9921f21074da736fa124c87
Wesley <wesleywijsen@hotmail.com>
2018-12-21 13:21:38 +0100

    styling

---
+++ includes/nav.inc.php
@@ -0,0 +1,8 @@
+    <nav>
+        <ul>
+            <li id="logo"></li>
+            <li> <a href="#">Overzicht</a> </li>
+            <li>  <a href="#">Profiel</a>  </li>
+            <li><a href="create_activity.php">Activiteit aanmaken</a></li>
+        </ul>
+    </nav>
